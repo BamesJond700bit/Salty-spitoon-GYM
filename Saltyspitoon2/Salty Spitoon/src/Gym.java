7b469cf2dcaec59b2c0d6c884a99a6fb5e795ca7
BamesJond700bit <121757866+BamesJond700bit@users.noreply.github.com>
2023-06-19 22:42:28 +0800

    memperbaiki delete

--- Saltyspitoon2/Salty Spitoon/src/Gym.java
+++ Saltyspitoon2/Salty Spitoon/src/Gym.java
@@ -5,16 +5,17 @@ class Gym extends Member {
     public ArrayList<Member> members = new ArrayList<>();
     public ArrayList<Suplemen> suplemens = new ArrayList<>();
 
-
-    public void addMember(Member member) {
-        if (!isMemberIdExist(member.getId())) {
-            this.members.add(member);
+//untuk memastikan tidak ada duplikasi id pada member
+    public void addMember(Member member) { //menerima objek member sebagai parameter
+        if (!isMemberIdExist(member.getId())) { //untuk mengecek apakah id sudah ada didalam members
+            this.members.add(member); //jika id false maka akan ditambahkan
+                                      // dan jika true maka akan mencetak pesan
         } else {
             System.out.println("Member Sudah ada !");
         }
     }
 
-    public boolean isMemberIdExist(int id) {
+    public boolean isMemberIdExist(int id) { //menerima parameter id
         for (Member member : this.members) {
             if (member.getId()==(id)) {
                 return true;
@@ -22,7 +23,18 @@ class Gym extends Member {
         }
         return false;
     }
+    //Melakukan perulangan untuk setiap member dalam daftar members.
+    //Pada setiap iterasi, memeriksa apakah id member saat ini
+    // sama dengan id yang sedang diperiksa.
+    //Jika id member ditemukan dalam daftar members, mengembalikan
+    // true yang menunjukkan bahwa id member tersebut sudah ada.
+    //Jika tidak ada id member yang cocok dengan
+    // parameter yang diberikan, mengembalikan false yang menunjukkan bahwa
+    // id member tersebut belum ada dalam daftar members.
 
+
+
+//untuk memastikan tidak ada duplikasi id pada suplemen
     public void addSuplemen(Suplemen suplemen) {
         if (!isSuplemenIdExist(suplemen.id)) {
             this.suplemens.add(suplemen);
@@ -43,11 +55,12 @@ class Gym extends Member {
     @Override
     public void Jadwal() {
         System.out.println("============================");
-        System.out.println("  JADWAL PELATIHAN YOGA     ");
+        System.out.println("   JADWAL PELATIHAN YOGA    ");
         System.out.println("============================");
         System.out.println("|Senin  |   16.00–18.00    |");
         System.out.println("|Rabu   |   16.00–18.00    |");
         System.out.println("|Jumat  |   10.00–12.00    |");
         System.out.println("============================");
     }
+
 }
--- Saltyspitoon2/Salty Spitoon/src/Main.java
+++ Saltyspitoon2/Salty Spitoon/src/Main.java
@@ -2,19 +2,20 @@ import java.util.Scanner;
 public class Main {
 
     static Scanner scan = new Scanner(System.in);
-    static Gym gym = new Gym();
+
+    static Gym gym = new Gym(); //    membuat inisiasi object gym dari kelas Gym
 
     public static void main(String[] args) {
-        initGymData();
+        initGymData(); //memberikan data awal
 
         String isContinue = "y";
 
-        while (isContinue.equals("y")) {
-            showMenu();
-            int selectedMenu = chooseMenu();
+        while (isContinue.equals("y")) { //perulangan akan terus jalan jika iscontinue bernilai y
+            showMenu();  //memanggil fungsi show menu
+            int selectedMenu = chooseMenu(); //memanggil fungsi chooseMenu yang mengembalikan pilihan menu
 
             try {
-                switch (selectedMenu) {
+                switch (selectedMenu) { //memeriksa pilihan menu
                     case 1:
                         showMembers();
                         break;
@@ -45,33 +46,35 @@ public class Main {
                     default:
                         throw new IllegalArgumentException("input yang bener bang");
                 }
-            } catch (Exception e) {
+            } catch (Exception e) {  //jika terjadi kesalahan akan ditangkap dan menampilkan pesan eror
                 System.out.println("Error: " + e.getMessage());
             }
 
 
             System.out.print("continue ? (y/n)");
             isContinue = scan.next();
+            //jika Y maka perulangan akan berlanjut dan N untuk menghentikan perulangan
         }
 
     }
 
-//    method untuk menampilan menu
-    public static void showMenu() {
-        System.out.println("==========SALTY SPITOON=============");
-        System.out.println("1.Show Member");
-        System.out.println("2.Add Member");
-        System.out.println("3.Update Member");
-        System.out.println("4.Delete Member");
-        System.out.println("5.Add Suplemen");
-        System.out.println("6.Show Suplemen");
-        System.out.println("7.Sell Suplemen");
-        System.out.println("8.Gym Trainer");
-        System.out.println("9.Jadwal Gym");
+    public static void showMenu() { //    method untuk menampilan menu
+        System.out.println("====================================");
+        System.out.println("||         SALTY SPITOON          ||");
+        System.out.println("====================================");
+        System.out.println("|1|          Show Member          ||");
+        System.out.println("|2|          Add Member           ||");
+        System.out.println("|3|         Update Member         ||");
+        System.out.println("|4|         Delete Member         ||");
+        System.out.println("|5|         Add Suplemen          ||");
+        System.out.println("|6|         Show Suplemen         ||");
+        System.out.println("|7|         Sell Suplemen         ||");
+        System.out.println("|8|         Gym Trainer           ||");
+        System.out.println("|9|          Jadwal Gym           ||");
         System.out.println("=====================================");
     }
 
-    public static void initGymData() {
+    public static void initGymData() {  //method untuk mengisialisasi data awal
         Member member1 = new Member();
         member1.setId(1);
         member1.setName("Vale");
@@ -80,7 +83,7 @@ public class Main {
         member2.setId(2);
         member2.setName("Evan");
 
-        Member member3 = new Member();
+        Member member3 = new Member();    //membuat object member
         member3.setId(3);
         member3.setName("Dika");
 
@@ -90,7 +93,7 @@ public class Main {
         suplemen1.harga = "Rp. 79.000";
 
         Suplemen suplemen2 = new Suplemen();
-        suplemen2.id = 2;
+        suplemen2.id = 2;                     //membuat object suplemen
         suplemen2.name = "Creatine";
         suplemen2.harga = "Rp. 100.000";
 
@@ -101,39 +104,42 @@ public class Main {
 
 
         gym.members.add(member1);
-        gym.members.add(member2);
+        gym.members.add(member2);       //menambahkankan member kedalam arraylist
         gym.members.add(member3);
 
         gym.suplemens.add(suplemen1);
-        gym.suplemens.add(suplemen2);
+        gym.suplemens.add(suplemen2);    //menambah suplemen ke dalam arraylist
         gym.suplemens.add(suplemen3);
 
     }
 
 
-    public static int chooseMenu() {
+    public static int chooseMenu() {  //untuk menampilkan,membaca dan mengembalikan pilihan
         System.out.print("choose menu : ");
         int pilihan = scan.nextInt();
         return pilihan;
     }
 
-    public static void showMembers() {
-        for (Member member : gym.members) {
-            System.out.println(member.getId() + " " + member.getName());
-        }
+    public static void showMembers() {  //untuk menampilkan member
+        int i=0;  //inisialisasi nomor urut
+        for (Member member : gym.members) {  //mengiterasi objek member di dalam arraylist members
+            System.out.println(++i + " " + member.getName());
+        } //mencetak nomor urut nama anggota
     }
 
-    public static void addMember() {
-        Member member = new Member();
+    public static void addMember() { //untuk menambahkan member
+        Member member = new Member(); //membuat objek member
 
         System.out.print("id : ");
-        int id = scan.nextInt();
-        member.setId(id);
+        int id = scan.nextInt();  //menginput id
+        member.setId(id); //menset id
 
-        System.out.print("name : ");
-        String name = scan.next();
-        member.setName(name);
-        gym.addMember(member);
+        System.out.print("nama : ");
+        String name = scan.next(); //meninput nama
+        member.setName(name); //menset name
+        gym.addMember(member); //memanggil method dari objek gym
+                               //untuk menambahkan objek member
+                              // baru ke dalam Arraylist members
     }
 
     public static void updateMember() {
@@ -141,27 +147,48 @@ public class Main {
         int id = scan.nextInt();
         System.out.println("Masukkan id baru :");
         int idNew = scan.nextInt();
+        boolean idExists = false;
+
         for (Member member : gym.members) {
-            if (member.getId() == id) {
-                System.out.println("Id sudah ada!");
+            if (member.getId() == idNew) {
+                idExists = true;
                 break;
             }
+        }
+
+        if (idExists) {
+            System.out.println("Id sudah ada!");
+        } else {
             System.out.println("Masukkan Nama Baru :");
             scan.nextLine();
             String nameNew = scan.nextLine();
-            Member barangedit = gym.members.get(id - 1);
-            barangedit.setId(idNew);
-            barangedit.setName(nameNew);
-            gym.members.set(id - 1, barangedit);
-
+            Member memberedit = gym.members.get(id - 1);
+            memberedit.setId(idNew);
+            memberedit.setName(nameNew);
+            gym.members.set(id - 1, memberedit);
         }
-
     }
     public static void deleteMember() {
-        System.out.println("Masukkan id member yang ingin dihapus :");
+        showMembers();
+        System.out.println("Masukkan id member yang ingin dihapus:");
         int id = scan.nextInt();
-        gym.members.remove(id - 1);
+
+        Member memberToDelete = null;
+        for (Member member : gym.members) {
+            if (member.getId() == id) {
+                memberToDelete = member;
+                break;
+            }
+        }
+
+        if (memberToDelete != null) {
+            gym.members.remove(memberToDelete);
+            System.out.println("Member berhasil dihapus.");
+        } else {
+            System.out.println("Member dengan ID tersebut tidak ditemukan.");
+        }
     }
+
 
     public static void addSuplemen(){
         Suplemen suplemen = new Suplemen();
@@ -175,23 +202,38 @@ public class Main {
     }
 
     public static void showSuplemens() {
+        int i = 0;
         for (Suplemen suplemen : gym.suplemens) {
-            System.out.println(suplemen.id + " " + suplemen.name);
+            System.out.println(++i+" "+ suplemen.name);
             System.out.println(suplemen.harga);
         }
     }
 
     public static void sellSuplemen() {
-        System.out.println("Masukkan id suplemen yang ingin dijual :");
+        showSuplemens();
+        System.out.println("Masukkan id suplemen yang ingin dijual:");
         int id = scan.nextInt();
-        gym.suplemens.remove(id - 1);
-        if(gym.suplemens==null){
-            System.out.println("STOK QUASONG !");
-        }else{
-            showSuplemens();
+
+        int index = -1;
+        for (int i = gym.suplemens.size() - 1; i >= 0; i--) {
+            Suplemen suplemen = gym.suplemens.get(i);
+            if (suplemen.id == id) {
+                index = i;
+                break;
+            }
+        }
+
+        if (index != -1) {
+            gym.suplemens.remove(index);
+            System.out.println("Suplemen berhasil dijual.");
+        } else {
+            System.out.println("Suplemen dengan ID tersebut tidak ditemukan.");
         }
     }
-   public static void gymTrainer(){
+
+
+
+    public static void gymTrainer(){
 
         Gymtrainer trainer1 = new FitnessTrainer("Larry",5);
        Gymtrainer trainer2 = new Yogatrainer("Sandy" ,10);
@@ -209,6 +251,7 @@ public class Main {
        trainer2.sesiPelatihan();
    }
 
+//   memanggil method override
    public static void jadwalGym(){
         Member jadwal1 = new Member();
        jadwal1.Jadwal();
